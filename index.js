3f0fc4c68b52c2d3a7d77a852125d36c6c695cf4
Isaac-Machini <isaac.machini@student.moringaschool.com>
2022-08-19 21:34:49 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = 62
+let random = 6
+Number.isInterger = true
+let max = (4,8,12,16,20)
+let num3 = 14
+let num4 = 5
+let mod = 4
